b543359439d8022d6d786e5aa62fe0db5020442a
itguang <1445037803@qq.com>
2017-09-29 10:50:45 +0800

    初始化

---
+++ test2/src/main/java/com/example/test2/Test2Application.java
@@ -0,0 +1,12 @@
+package com.example.test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test2Application.class, args);
+	}
+}
