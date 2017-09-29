97a09eb4de5cf71deaed3b51695aabfdae700937
itguang <1445037803@qq.com>
2017-09-29 13:14:04 +0800

    初始化

---
+++ springboot_01/src/main/java/com/example/springboot_01/Springboot01Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot01Application.class, args);
+	}
+}
