9416a1db09d5155497444edc5849f74e670032fb
itguang <1445037803@qq.com>
2017-09-29 10:55:59 +0800

    初始化

---
+++ test3/src/main/java/com/example/test3/Test3Application.java
@@ -0,0 +1,12 @@
+package com.example.test3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test3Application.class, args);
+	}
+}
